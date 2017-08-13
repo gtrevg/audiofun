587435a4165be100a42da25c3c6f456eca1b894c
Mariano Gappa <mariano@movio.co>
2017-08-13 20:04:16 +1200

    Implements concurrency model. Multiple sine waves.

--- main.go
+++ main.go
@@ -4,25 +4,36 @@ import (
 	"log"
 	"math"
 	"math/rand"
+	"sync"
 
 	"github.com/hajimehoshi/oto"
 )
 
+var argSampleRate = 11025
+
 func main() {
 	var (
-		argSampleRate = 11025
-		argDuration   = 0.2
+		out = make(chan []uint8)
+		chs = make([]chan []uint8, 3)
+	)
+	chs[0] = make(chan []uint8)
+	chs[1] = make(chan []uint8)
+	chs[2] = make(chan []uint8)
+	// chs[3] = make(chan []uint8)
+	go gen1(chs[0])
+	go gen1(chs[1])
+	go gen1(chs[2])
+	// go gen1(chs[3])
+	go agg(chs, out)
+	play(out)
+}
+
+func gen1(ch chan []uint8) {
+	var (
+		argDuration = 0.3
 		// argVolume     = 10000
 		// argFrequency = 440.0
 	)
-	p, err := oto.NewPlayer(argSampleRate, 1, 1, 256)
-	if err != nil {
-		log.Fatal("halp", err)
-	}
-	defer p.Close()
-	p.SetUnderrunCallback(func() {
-		log.Println("UNDERRUN, YOUR CODE IS SLOW")
-	})
 
 	var (
 		samplesPerSecond = uint32(argSampleRate)                           // The number of samples per second
@@ -44,7 +55,51 @@ func main() {
 			// fmt.Println(".")
 			// time.Sleep(5 * time.Millisecond)
 		}
+		ch <- waveform
+	}
+}
 
-		p.Write(waveform)
+func agg(chs []chan []uint8, out chan []uint8) {
+	for {
+		var (
+			wg  sync.WaitGroup
+			ins = make([][]uint8, len(chs))
+		)
+		for i, ch := range chs {
+			wg.Add(1)
+			go func(ch chan []uint8, i int) {
+				defer wg.Done()
+				ins[i] = <-ch
+			}(ch, i)
+		}
+		wg.Wait()
+		out <- doAgg(ins)
+	}
+}
+
+func doAgg(ins [][]uint8) []uint8 {
+	var o = make([]uint8, len(ins[0]))
+	for i := range ins[0] {
+		var a int
+		for j := range ins {
+			a += int(ins[j][i])
+		}
+		o[i] = uint8(a / len(ins))
+	}
+	return o
+}
+
+func play(out chan []uint8) {
+	p, err := oto.NewPlayer(argSampleRate, 1, 1, 256)
+	if err != nil {
+		log.Fatal("halp", err)
+	}
+	defer p.Close()
+	p.SetUnderrunCallback(func() {
+		log.Println("UNDERRUN, YOUR CODE IS SLOW")
+	})
+
+	for o := range out {
+		p.Write(o)
 	}
 }
